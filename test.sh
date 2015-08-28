f9905a7ac615ef6a664c2f0582988bdf4e010480
Jesse A <jesse.arnett@inbox.com>
2015-08-28 18:28:13 -0400

    added new server list url from fatihusta devel branch

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export SPEEDTEST_1767="{'name': 'Atlanta, GA', 'url': 'http://sto-smtn-01.sys.comcast.net/speedtest/upload.php', 'country': 'United States', 'lon': '-84.3881', 'cc': 'US', 'host': 'sto-smtn-01.sys.comcast.net:8080', 'sponsor': 'Comcast', 'url2': 'http://a-sto-smtn-01.sys.comcast.net/speedtest/upload.php', 'lat': '33.7489', 'id': '1767', 'd': 0.00}"
+#export SPEEDTEST_1767="{'name': 'Atlanta, GA', 'url': 'http://sto-smtn-01.sys.comcast.net/speedtest/upload.php', 'country': 'United States', 'lon': '-84.3881', 'cc': 'US', 'host': 'sto-smtn-01.sys.comcast.net:8080', 'sponsor': 'Comcast', 'url2': 'http://a-sto-smtn-01.sys.comcast.net/speedtest/upload.php', 'lat': '33.7489', 'id': '1767', 'd': 0.00}"
+SPEEDTEST_1767="{'name': 'Atlanta, GA', 'url': 'http://sto-smtn-01.sys.comcast.net/speedtest/upload.php', 'country': 'United States', 'lon': '-84.3881', 'cc': 'US', 'host': 'sto-smtn-01.sys.comcast.net:8080', 'sponsor': 'Comcast', 'url2': 'http://a-sto-smtn-01.sys.comcast.net/speedtest/upload.php', 'lat': '33.7489', 'id': '1767', 'd': 0.00}"
 
 ./speedtest_cli.py --server 1767 # a server we've overridden above
 #./speedtest_cli.py --server 1000 # a server we haven't overriden
